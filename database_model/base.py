1e32aa4115dc55ff575d4867c32ab4a9eb589226
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-10-29 10:21:07 +0200

    Define basic class fro time added.

--- database_model/base.py
+++ database_model/base.py
@@ -1,12 +1,13 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import MetaData
+from sqlalchemy import Column, DateTime, func
 
 naming_convention = {
-  "ix": "ix_%(column_0_label)s",
-  "uq": "uq_%(table_name)s_%(column_0_name)s",
-  "ck": "ck_%(table_name)s_%(constraint_name)s",
-  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-  "pk": "pk_%(table_name)s"
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
 }
-Base = declarative_base(metadata=MetaData(schema='abs', naming_convention=naming_convention))
 
+
+class InsertionDateTime:
+    time_added = Column(DateTime, server_default=func.now(), nullable=False)
