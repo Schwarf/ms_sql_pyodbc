9153d2143fdb5c1fbaace082a0453af02e984f94
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-10-29 10:36:36 +0200

    Renaming.

--- database_model/scheme_definitions.py
+++ database_model/scheme_definitions.py
@@ -1,7 +1,7 @@
 from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-from base import naming_convention
+from basics import naming_convention
 
 BaseDefinition = declarative_base(metadata=MetaData(naming_convention=naming_convention))
 metadata = BaseDefinition.metadata
