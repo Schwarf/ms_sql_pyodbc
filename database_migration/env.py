1da412ee3a9ff0e77b485f7fe61d471434a1e724
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-10-30 11:22:10 +0100

    Fix env.

--- database_migration/env.py
+++ database_migration/env.py
@@ -23,6 +23,14 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = [SchemeDefinition.metadata]
 
+sqlalchemy_env_variable = "SQLALCHEMY_URL"
+if sqlalchemy_env_variable not in os.environ:
+    raise ValueError(f"Environment variable: {sqlalchemy_env_variable} not set for alembic migration!")
+
+config.set_main_option("sqlalchemy_url", os.environ[sqlalchemy_env_variable])
+
+
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
