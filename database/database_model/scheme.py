65224cba7b91769d687b6bc5b5f788d3226d96f4
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-10-31 17:48:33 +0100

    Migrate to minimal scheme.

--- database/database_model/scheme.py
+++ database/database_model/scheme.py
@@ -4,24 +4,13 @@ from database.database_model.basic_scheme_definitions import InsertionDateTime
 from database.database_model.scheme_definitions import SchemeDefinition, metadata
 
 
-class RootWebsites(SchemeDefinition, InsertionDateTime):
-    __tablename__ = "RootWebsites"
-
-    internal_id = Column(Integer, primary_key=True)
-    url_hash = Column(Integer, nullable=False, index=True, unique=True)
-    url = Column(String(1000), nullable=False)
-
-
 class VisitedWebsites(SchemeDefinition, InsertionDateTime):
     __tablename__ = "VisitedWebsites"
 
     internal_id = Column(Integer, primary_key=True)
-    root_website_id = Column(ForeignKey("RootWebsites.internal_id"))
     url_hash = Column(Integer, nullable=False, index=True, unique=True)
     url = Column(String(1000), nullable=False)
+    root_url = Column(String(1000), nullable=False)
+    root_url_hash = Column(Integer, nullable=False)
+    keywords = Column(String())
 
-
-WebsiteMetadata = Table("WebsiteMetadata", metadata,
-    Column('website_id', ForeignKey("VisitedWebsites.internal_id")),
-    Column('title', String(1000), nullable=False),
-    Column('contained_urls', String()))
