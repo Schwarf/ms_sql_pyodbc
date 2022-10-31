02b035b35e7536cd105b97240f66ca125566c832
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-10-31 14:21:43 +0100

    Split and refactor code.

--- connect_to_database.py
+++ connect_to_database.py
@@ -1,55 +1,9 @@
 import argparse
-import os
 import sys
 
-import alembic.command
-import alembic.config
-import pyodbc
 
 
-def get_pyodbc_connection_string(password_file: str) -> str:
-    driver = "ODBC Driver 17 for SQL Server"
-    server = 'localhost'
-    database = 'Test'
-    with open(password_file, "r") as file:
-        lines = file.read().splitlines()
-        username = lines[0]
-        password = lines[1]
-        file.close()
-    url = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
-    return url
 
-
-def get_sql_alchemy_url(password_file: str) -> str:
-    with open(password_file, "r") as file:
-        lines = file.read().splitlines()
-        username = lines[0]
-        password = lines[1]
-        file.close()
-    server = 'localhost'
-    database = 'Test'
-    driver = "ODBC+Driver+17+for+SQL+Server"
-    url = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}"
-    return url
-
-
-def set_sql_alchemy_env_variable(password_file: str) -> None:
-    url = get_sql_alchemy_url(password_file)
-    sqlalchemy_env_variable = "SQLALCHEMY_URL"
-    os.environ[sqlalchemy_env_variable] = url
-    print(os.environ.get(sqlalchemy_env_variable))
-
-
-def get_database_connection(password_file: str) -> pyodbc.Connection:
-    url = get_pyodbc_connection_string(password_file)
-    connection = pyodbc.connect(url)
-    return connection
-
-
-def upgrade_database_scheme(password_file: str) -> None:
-    alembic_config = alembic.config.Config("alembic.ini")
-    set_sql_alchemy_env_variable(password_file)
-    alembic.command.upgrade(config=alembic_config, revision="head")
 
 
 def main() -> None:
---
+++ database_migration/migration.py
@@ -0,0 +1,27 @@
+import os
+import alembic.command
+import alembic.config
+
+# TODO This function only updates the head, but does not autogenerate the revision
+def upgrade_database_scheme(password_file: str) -> None:
+    alembic_config = alembic.config.Config("alembic.ini")
+    set_sql_alchemy_env_variable(password_file)
+    alembic.command.upgrade(config=alembic_config, revision="head")
+
+def set_sql_alchemy_env_variable(password_file: str) -> None:
+    url = get_sql_alchemy_url(password_file)
+    sqlalchemy_env_variable = "SQLALCHEMY_URL"
+    os.environ[sqlalchemy_env_variable] = url
+    print(os.environ.get(sqlalchemy_env_variable))
+
+def get_sql_alchemy_url(password_file: str) -> str:
+    with open(password_file, "r") as file:
+        lines = file.read().splitlines()
+        username = lines[0]
+        password = lines[1]
+        file.close()
+    server = 'localhost'
+    database = 'Test'
+    driver = "ODBC+Driver+17+for+SQL+Server"
+    url = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}"
+    return url
