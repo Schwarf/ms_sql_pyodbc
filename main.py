5f6bf4debb7a0b6d86aa30752ff3ebfcec882e75
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-11-06 18:49:06 +0100

    LIP scrapy from script.

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import argparse
 import sys
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy_crawler.bookscraper.bookscraper.spiders.bookscraper import BookScraper
+from scrapy_crawler.bookscraper.bookscraper.pipelines import BookscraperPipeline
+from scrapy.utils.project import get_project_settings
 from database_access.access import get_database_connection
+
+def _create_engine(self):
+    engine = scrapy.core.engine.ExecutionEngine(self, lambda _: self.stop())
+    engine.scraper.itemproc._add_middleware(BookscraperPipeline()) #loads item pipeline
+    #engine.donwloader.middleware._add_middleware(downloader_middleware_class)
+    return engine
+
+def main2() -> None:
+    spider = BookScraper()
+    scrapy.crawler.Crawler._create_engine = _create_engine
+    process = CrawlerProcess()
+    settings = get_project_settings()
+    crawler = Crawler(settings)
+
+    process.crawl(BookScraper)
+    process.start()
+
 
 def main() -> None:
     argument_parser = argparse.ArgumentParser()
@@ -23,4 +45,4 @@ def main() -> None:
     print(splits," ",  number_of_copies)
 
 if __name__ == "__main__":
-    main()
+    main2()
