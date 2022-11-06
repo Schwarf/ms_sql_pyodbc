9da45d609e4008bee2283acc60a64de804d447b2
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-11-06 11:53:43 +0100

    LIP

--- scrapy_crawler/bookscraper/bookscraper/pipelines.py
+++ scrapy_crawler/bookscraper/bookscraper/pipelines.py
@@ -5,9 +5,19 @@
 
 
 # useful for handling different item types with a single interface
+import json
 from itemadapter import ItemAdapter
 
 
 class BookscraperPipeline:
+    def open_spider(self, spider):
+        self._json_file = open('book_items.json', 'w')
+
+    def close_spider(self, spider):
+        self._json_file.close()
+
     def process_item(self, item, spider):
+        print("HALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOO")
+        line = json.dumps(ItemAdapter(item).asdict()) + "\n"
+        self._json_file.write(line)
         return item
--- scrapy_crawler/bookscraper/bookscraper/spiders/bookscraper.py
+++ scrapy_crawler/bookscraper/bookscraper/spiders/bookscraper.py
@@ -10,10 +10,10 @@ class BookScraper(CrawlSpider):
     start_urls = ["http://books.toscrape.com"]
 
     rules = (Rule(LinkExtractor(restrict_css=".nav-list > li > ul > li > a"), follow=True),
-             Rule(LinkExtractor(restrict_css=".product_pod > h3 > a"), callback="parse_book")
+             Rule(LinkExtractor(restrict_css=".product_pod > h3 > a"), callback="parse")
              )
 
-    def parse_book(self, response):
+    def parse(self, response):
         book_item = BookItem()
         book_item["image_url"] = response.urljoin(response.css(".item.active > img::attr(src)").get())
         book_item["title"] = response.css(".col-sm-6.product_main > h1 ::text").get()
@@ -21,13 +21,4 @@ class BookScraper(CrawlSpider):
         book_item["upc"] = response.css(".table.table-striped > tr:nth-child(1) > td::text").get()
         book_item["availability"] = response.css(".table.table-striped > tr:nth-child(6) > td::text").get()
         book_item["url"] = response.url
-        data = []
-        data.append(book_item)
-        filename = "test_result.json"
-        with open(filename, 'w') as json_file:
-            json.dump(data, json_file,
-                      indent=4,
-                      separators=(',', ': '))
-
-
-        return book_item
+        yield book_item
