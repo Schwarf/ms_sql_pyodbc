89334318b3b87fc96835ac174fcf19cafa2ea4a3
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-11-06 18:16:09 +0100

    Add books table with alembic.

--- database/database_model/scheme.py
+++ database/database_model/scheme.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Table
+from sqlalchemy import Column, Integer, String
 
 from database_model.basic_scheme_definitions import InsertionDateTime
-from database_model.scheme_definitions import SchemeDefinition, metadata
+from database_model.scheme_definitions import SchemeDefinition
 
 
 class VisitedWebsites(SchemeDefinition, InsertionDateTime):
@@ -13,3 +13,16 @@ class VisitedWebsites(SchemeDefinition, InsertionDateTime):
     root_url_hash = Column(Integer, nullable=False)
     keywords = Column(String())
 
+
+class TestBooks(SchemeDefinition, InsertionDateTime):
+    __tablename__ = "books"
+    book_id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    price = Column(String(10), nullable=False)
+    upc = Column(String(20), nullable=False)
+    availability = Column(String(20), nullable=False)
+    number_of_copies = Column(Integer, nullable=False)
+    url = Column(String(1000), nullable=False)
+    image_url = Column(String(1000), nullable=False)
+
+    keywords = Column(String())
